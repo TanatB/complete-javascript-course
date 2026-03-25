1f5dfd6b97af1fec0170b2855caeed1ef132a872
TanatB <bright.tanat@hotmail.com>
2026-03-25 14:43:40 +0700

    Noted on 93. High-Level overview of JavaScript.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// JavaScript Definition
+
+// 1. High-Level
+
+// 2. Garbage-collected
+
+// 3. Interpreted or just-in-time compiled
+
+// 4. Multi-paradigm
+// 4.1 Procedural Programming
+// 4.2 OOP
+// 4.3 Functional Programming
+
+// 5. Prototype-based Object-Oriented
+
+// 6. First-class Functions (Treated functions as variables)
+
+// 7. Dynamically Typed
+
+// 8. Single-threaded
+
+// 9. Non-blocking event loop
